9bf4756c18664b64b463b4d229f5f69ad9df1cf4
Skip Huang <huangzpp@pku.edu.cn>
2020-01-14 14:50:03 +0000

    solve

--- eth/handler.go
+++ eth/handler.go
@@ -70,13 +70,8 @@ type ProtocolManager struct {
 	networkID  uint64
 	forkFilter forkid.Filter // Fork ID filter, constant across the lifetime of the node
 
-<<<<<<< HEAD
-	fastSync    uint32 // Flag whether fast sync is enabled (gets disabled if we already have blocks)
-	acceptTxs   uint32 // Flag whether we're considered synchronised (enables transaction processing)
-=======
 	fastSync  uint32 // Flag whether fast sync is enabled (gets disabled if we already have blocks)
 	acceptTxs uint32 // Flag whether we're considered synchronised (enables transaction processing)
->>>>>>> 57420db5ed2ac83607dda2f605e0fb658d15cb3b
 
 	checkpointNumber uint64      // Block number for the sync progress validator to cross reference
 	checkpointHash   common.Hash // Block hash for the sync progress validator to cross reference
